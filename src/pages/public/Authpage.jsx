d5cf3f3f1058e61b0d15598f537d71d471b550dc
Chime Juliet Somtochukwu <annychime@gmail.com>
2025-08-27 20:32:20 +0100

    feat: adds auth layout

--- src/pages/public/Authpage.jsx
+++ src/pages/public/Authpage.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { Col, Flex, Row } from "antd";
+import reelpayAuthBg from "../../assets/images/img/reelpayauthbg.png";
 
 const Authpage = () => {
-  return <div>Authpage</div>;
+  return (
+    <Row className="h-screen min-h-screen border border-amber-900">
+      <Col xs={24} lg={16}>
+        <div className="h-screen">
+          <div>lkll</div>
+          <Flex justify="center" align="center" className="border">
+            <div className="border border-amber-600 w-95 max-w-95">hjhjh</div>
+          </Flex>
+        </div>
+      </Col>
+      <Col
+        xs={0}
+        lg={8}
+        className="h-screen fixed right-0 top-0 w-full overflow-auto"
+      >
+        <div
+          className={` h-screen bg-cover bg-no-repeat`}
+          style={{
+            backgroundImage: `url(${reelpayAuthBg})`,
+            backgroundPosition: "right center",
+          }}
+        />
+      </Col>
+    </Row>
+  );
 };
 
 export default Authpage;
